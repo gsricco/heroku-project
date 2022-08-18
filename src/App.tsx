f03b9cd0275e609f7cf4a00f6e58ea2af531b2ca
SergioGord <gsricco@yandex.ru>
2022-08-18 18:06:57 +0300

    add dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>SergioGord</h1>
+        <h1>SergioGord -- dev branch</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
